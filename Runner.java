5f6439b67778afe86360b15123a6af5f03220a48
Anime-CoolCode <singh.animesh.tanwar@gmail.com>
2024-07-14 22:41:09 +0530

    Removed the .class file and added the Varible part

--- Runner.java
+++ Runner.java
@@ -1,6 +1,9 @@
 
 public class Runner{
     public static void main(String[] args){
+        int x = 0;
+        int y = 8;
+        System.out.println("X + Y = "+(x+y));
         System.out.println("Runner is fine");
     }
 }
